c57904dbdb566607ab7496b38117d7fd38287e64
MBNascimento <124750651+MBNascimento@users.noreply.github.com>
2025-06-20 09:05:49 -0300

    Aulas do dia 20/06/2025

---
+++ Aula 1.py
@@ -0,0 +1,12 @@
+print()
+
+# a serquilha, permite escrever um comentário na mema linha
+# o intrpretador não lê.
+
+print(1 + 1)
+
+""" Docstrings não é um comentário oficialmente mas é muito,
+    usado como comentário multilinha,
+    a docstring é lida pelo interpretador ."""
+
+print(1, 2, 3, 4, 5)
