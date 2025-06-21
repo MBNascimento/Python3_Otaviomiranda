ecf81b002a3e94e97a3957f236fe386e093ea688
MBNascimento <124750651+MBNascimento@users.noreply.github.com>
2025-06-20 22:39:23 -0300

    Aulas dia 20/06/2025

--- Aula2.py
+++ Aula2.py
@@ -1,2 +1,26 @@
 print(12, 34)
-print(56, 78)
+print(56, 78)  # esse exemplo de print terá como saída os núneros
+# sem separadores,
+# apenas com espaços.
+
+print(12, 34, sep="Jõao")
+# esse código esta com argumento nomeado sep=()
+
+print(56, 78, sep='-', end='\n')
+# se usar o end='\n' nada muda pois ja é padrão
+
+print(12, 34, sep='-', end='##')
+""" mas se usar o end='com qualquer caracter
+ o caracter é lido e deixa de ser quebrada a linha """
+
+print(56, 78, sep='-', end='\n')
+
+print(12, 34, sep='-', end='##\n')
+
+print(56, 78, sep='-', end='\n##')
+
+print(9, 10, sep='-', end=' FIM '*3)
+# nesse caso a saída tem escrito FIM três veses.
+
+# \r \n -> CRLF  é um sistema padrão de quebra de linha no Windows
+# \n  -> LF  é um sistema padão de quebra de linha no sistema baseado em iunix.
---
+++ Aula3.py
@@ -0,0 +1,30 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dimâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+""" o python por ser linguagem Dinâmica reconhece os números com inteiros"""
+
+# Aspas simples
+print('Marcio Nascimento')
+
+# Aspas duplas
+print("Marcio Nascimento")
+
+# Escape
+print("Marcio \"Nascimento")
+"""o caracter \ escape indica para o inteerpretador não ler o proximo caracter
+o que  permite que seja impresso a aspas dupla"""
+
+# r
+print(r"Marcio \"Nascimento\"")
+"""com o caracter r antes das Aspas idica que eu quero que
+os caracteres de \escape apareça no print"""
+
+print("Marcio 'Barbosa' Nascimento")
+# se eu quiser imprimir aspas simples começo com aspas Duplas
+
+print('Marcio "Barbosa" Nascimento')
+# se eu quiser imprimir aspas duplas começo com aspas simples
